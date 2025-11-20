b38495f915cc3137a1fcc4fa1b235304a4338a08
Maggie Chang <maggie.chang27@student.tamdistrict.org>
2025-11-20 13:22:19 -0800

    fleshed fillLayout and added printLayout

--- Card.java
+++ Card.java
@@ -1,11 +1,25 @@
 public class Card {
-    private int num;
-    private boolean isFlipped;
+    private boolean isFlipped = false;
+    private boolean DEBUG_MODE = true;
+    private boolean hasSymbol = false;
 
     public static String CARD_DISPLAY = "*";
-    public static String[] SYMBOL_DISPLAY;
+    public static String SYMBOL_DISPLAY;
 
-    public Card(String a, boolean b) {
-        
+    public Card(String symbol, boolean hasSymbol) {
+        symbol = SYMBOL_DISPLAY;
+        this.hasSymbol = hasSymbol;
+    }
+
+    public String getSymbol() {
+        return SYMBOL_DISPLAY;
+    }
+
+    public boolean getIsFlipped() {
+        return isUncovered;
+    }
+
+    public void setIsFlipped() {
+        isUncovered = true;
     }
 }
--- Layout.java
+++ Layout.java
@@ -1,16 +1,52 @@
+import java.util.ArrayList;
+
 public class Layout {
     private Card[][] layout = new Card[6][4];
+    
+    public ArrayList<String> symbolList = new ArrayList<String>("✦", "✧", "✰", "▣", "❤", "♡", "❄", "◯", "◎", "☢", "⇦", "⇨",
+     "✦", "✧", "✰", "▣", "❤", "♡", "❄", "◯", "◎", "☢", "⇦", "⇨");
 
     public Layout() {
         fillLayout();
     }
 
+    // fills each card with a symbol (each symbol is repeated once)
+    // then removing the symbol from symbolList.
+    // returns the filled layout with symbols added to each card.
     public Card[][] fillLayout() {
         for (Card[] card : layout) {
             for (int j = 0; j < layout[0].length; j++) {
-                card[j] = new Card("x", false);
+                int randomIndex = (int) (Math.random() * (symbolList.size()-1));
+                card[j] = new Card(symbolList[randomIndex], true);
+                symbolList[randomIndex].remove(randomIndex);
             }
         }
         return layout;
+    }
+
+    // this method is copied from on of my previous works with printing 2D arrays
+    public void printLayout() {
+        for (int k = 0; k <= layout[0].length-1; k++) { 
+            if (k == 0) {
+                System.out.print("    ");
+            }
+            System.out.print(k + "   ");
+        }
+        System.out.println();
+        for (int f = 0; f <= layout[0].length; f++) {
+            System.out.print("----");
+        }
+        System.out.println();
+
+        for (int i = 0; i < layout.length; i++) {
+            System.out.print(i + " | ");
+            for (int j = 0; j < layout[0].length; j++) {
+                //System.out.print(j);
+                System.out.print(layout[i][j] + "   ");
+            }
+            System.out.println();
+            System.out.println();
+        }
+
     }
 }
