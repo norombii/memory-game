ac1d5ce5c44128280b5ceac7d8804df09317c8ca
maggie <144709218+norombii@users.noreply.github.com>
2025-12-11 20:50:32 +0000

    working card!!!! (shows astericks to cover, symbol when "flipped")

--- GameRunner.java
+++ GameRunner.java
@@ -2,7 +2,7 @@ public class GameRunner {
     public static void main(String[] args) {
         Game game = new Game();
         game.play();
-        System.out.println("hi");
+        //System.out.println("hi");
 
     }
 }  
